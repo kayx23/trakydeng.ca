8f688e61816946af47119d659e10b82643b0f4d0
Traky <trakydeng@gmail.com>
2021-11-28 17:03:37 -0500

    some updates to the sidebar and general colour theme

--- config.js
+++ config.js
@@ -4,47 +4,34 @@ module.exports = {
   url: 'https://lumen.netlify.com',
   pathPrefix: '/',
   title: 'Traky Deng',
-  subtitle: 'MacU',
+  subtitle: 'Yet another tech fanatic',
   copyright: '© 2021 All Rights Reserved',
   disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  // googleAnalyticsId: 'UA-73379983-2',
   useKatex: false,
   menu: [
     {
-      label: 'Articles',
-      path: '/'
-    },
-    {
-      label: 'About me',
+      label: '.About()',
       path: '/pages/about'
     },
     {
-      label: 'Contact me',
-      path: '/pages/contacts'
+      label: '.Blog()',
+      path: '/'
+    },
+    {
+      label: '.Tags()',
+      path: '/tags'
     }
   ],
   author: {
     name: 'Traky Deng',
     photo: '/photo.jpg',
-    bio: 'Let\'s try this again',
+    bio: 'TBD',
     contacts: {
-      email: '',
-      facebook: '#',
-      telegram: '#',
-      twitter: '#',
-      github: '#',
-      rss: '',
-      vkontakte: '',
-      linkedin: '#',
-      instagram: '#',
-      line: '',
-      gitlab: '',
-      weibo: '',
-      codepen: '',
-      youtube: '',
-      soundcloud: '',
-      medium: '',
+      email: 'traky.deng@protonmail.com',
+      github: 'Kayx23',
+      linkedin: 'dengk7',
     }
   }
 };
