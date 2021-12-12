c204f9b380bbf3633649c7dcdf8493ac238855bf
Traky <trakydeng@gmail.com>
2021-12-12 01:01:20 -0500

    site social image config

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -18,8 +18,10 @@ const Layout = ({
   description,
   socialImage = ''
 }: Props) => {
-  const { author, url } = useSiteMetadata();
-  const metaImage = socialImage || author.photo;
+  const { url, siteSocialImage } = useSiteMetadata();
+  // socialImage is from posts
+  const metaImage = socialImage || siteSocialImage;
+  '/media/site-social-image.jpg'
   const metaImageUrl = url + metaImage;
 
   return (
@@ -34,28 +36,6 @@ const Layout = ({
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
         <meta name="twitter:image" content={metaImageUrl} />
-
-        // Tableau
-        {/* <script defer type='text/javascript'>
-          var divElement1 = document.getElementById('viz1638766620433');
-          var vizElement1 = divElement1.getElementsByTagName('object')[0];
-          vizElement1.style.width='1169px';
-          vizElement1.style.height='880px';
-
-          var divElement2 = document.getElementById('viz1638768106686');
-          var vizElement2 = divElement2.getElementsByTagName('object')[0];
-          vizElement2.style.width='1400px';
-          vizElement2.style.height='1060px';
-
-          var scriptElement1 = document.createElement('script');
-          scriptElement1.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
-          vizElement1.parentNode.insertBefore(scriptElement1, vizElement1);
-
-          var scriptElement2 = document.createElement('script');
-          scriptElement2.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
-          vizElement2.parentNode.insertBefore(scriptElement2, vizElement2);
-        </script> */}
-
       </Helmet>
       {children}
     </div>
